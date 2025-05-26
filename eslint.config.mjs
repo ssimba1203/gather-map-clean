693ecd6aa617855d553cb4959f0a11cccd468d9b
ssimba1203 <ssimba1203@gmail.com>
2025-05-26 16:26:29 +0900

    🔧 eslint any 타입 사용 허용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,16 @@
-import eslintPlugin from '@eslint/js';
-import tseslintPlugin from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import tseslint from '@typescript-eslint/eslint-plugin'
+import parser from '@typescript-eslint/parser'
 
 export default [
   {
-    name: 'my-custom-eslint-config',
     files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
-      parser: tsParser,
+      parser,
     },
     plugins: {
-      '@typescript-eslint': tseslintPlugin,
+      '@typescript-eslint': tseslint,
     },
     rules: {
-      // 여기에 룰 추가
       '@typescript-eslint/no-explicit-any': 'off',
     },
   },
